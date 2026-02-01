82955399014a2c828328d4ffa89da2bf7256d04d
KristianIhnat <kristian.ihnat@erstegroup.com>
2026-02-01 19:31:30 +0100

    Add getByTestId custom Cypress command

--- cypress/tests/login.spec.js
+++ cypress/tests/login.spec.js
@@ -1,28 +1,28 @@
 describe('Login functionality', () => {
     it('logs in successfully with valid credentials', () => {
         cy.visit('https://www.saucedemo.com')
-        cy.get('#user-name').type('standard_user')
-        cy.get('#password').type('secret_sauce')
-        cy.get('#login-button').click()
+        cy.getByTestId('username').type('standard_user')
+        cy.getByTestId('password').type('secret_sauce')
+        cy.getByTestId('login-button').click()
         cy.url().should('include', '/inventory.html')
     })
 
     it('shows error messages for invalid login', () => {
         cy.visit('https://www.saucedemo.com')
-        cy.get('#login-button').click()
-        cy.get('[data-test="error"]').should('contain', 'Username is required')
+        cy.getByTestId('login-button').click()
+        cy.getByTestId('error').should('contain', 'Username is required')
 
-        cy.get('#user-name').type('wrong_user')
-        cy.get('#login-button').click()
-        cy.get('[data-test="error"]').should('contain', 'Password is required')
+        cy.getByTestId('username').type('invalid user')
+        cy.getByTestId('login-button').click()
+        cy.getByTestId('error').should('contain', 'Password is required')
 
-        cy.get('#password').type('wrong_password')
-        cy.get('#login-button').click()
-        cy.get('[data-test="error"]').should('contain', 'Username and password do not match')
+        cy.getByTestId('password').type('invalid password')
+        cy.getByTestId('login-button').click()
+        cy.getByTestId('error').should('contain', 'Username and password do not match')
 
-        cy.get('#user-name').clear().type('locked_out_user')
-        cy.get('#password').clear().type('secret_sauce')
-        cy.get('#login-button').click()
-        cy.get('[data-test="error"]').should('contain', ' Sorry, this user has been locked out')
+        cy.getByTestId('username').clear().type('locked_out_user')
+        cy.getByTestId('password').clear().type('secret_sauce')
+        cy.getByTestId('login-button').click()
+        cy.getByTestId('error').should('contain', ' Sorry, this user has been locked out')
     })
 })
