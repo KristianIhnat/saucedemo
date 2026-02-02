4c02ba570e484a8a58133ea79cfc1ec1668ba806
KristianIhnat <kristian.ihnat@erstegroup.com>
2026-02-02 17:20:58 +0100

    Shows error messages for invalid checkout information

--- cypress/tests/createOrder.spec.ts
+++ cypress/tests/createOrder.spec.ts
@@ -3,7 +3,7 @@ beforeEach(() => {
 })
 
 describe('Cart & Checkout', () => {
-    it('add a product to the shopping cart', () => {
+    it('add a product to the shopping cart and create order', () => {
         cy.getByTestId('add-to-cart-sauce-labs-backpack').click()
         cy.getByTestId('shopping-cart-link').click()
         cy.url().should('include', '/cart.html')
@@ -17,5 +17,21 @@ describe('Cart & Checkout', () => {
         cy.getByTestId('finish').click()
         cy.getByTestId('complete-header')
             .should('have.text', 'Thank you for your order!')
+    })
+
+    it('shows error messages for invalid checkout information', () => {
+        cy.getByTestId('add-to-cart-sauce-labs-backpack').click()
+        cy.getByTestId('shopping-cart-link').click()
+        cy.getByTestId('checkout').click()
+        cy.getByTestId('continue').click()
+        cy.getByTestId('error').should('contain', 'First Name is required')
+
+        cy.getByTestId('firstName').type('Sherlock')
+        cy.getByTestId('continue').click()
+        cy.getByTestId('error').should('contain', 'Last Name is required')
+
+        cy.getByTestId('lastName').type('Holmes')
+        cy.getByTestId('continue').click()
+        cy.getByTestId('error').should('contain', 'Postal Code is required')
     })
 })
